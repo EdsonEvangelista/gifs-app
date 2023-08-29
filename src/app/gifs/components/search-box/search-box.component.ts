4e3ff711a25782dda943e037580b75b1777108f7
EdsonEvangelista <yokarc1989@gmail.com>
2023-08-29 07:43:40 -0500

    Proyecto Inicio

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar:</h5>
+    <input type="text"
+      class="form-control"
+      placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag( txtTagInput.value )"
+      #txtTagInput
+    >
+  `
+})
+export class SearchBoxComponent {
+
+
+  searchTag(newTag: string):void {
+    console.log({ newTag });
+
+  }
+}
