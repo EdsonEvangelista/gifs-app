05a6408b6237676ed746d2580109aa984cd1e7a9
EdsonEvangelista <yokarc1989@gmail.com>
2023-08-31 21:08:37 -0500

    Ultimo commit de agosto

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -17,6 +18,7 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
+    constructor(private gifsService: GifsService) {}
 
   // searchTag(newTag: string):void {
   //   console.log({ newTag });
@@ -26,7 +28,10 @@ export class SearchBoxComponent {
   searchTag():void {
     const newTag = this.tagInput.nativeElement.value;
 
-    console.log({ newTag });
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
+
 
   }
 }
